ca773ae85ce6fd4dec2c237c32973ac457add329
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-10-20 16:21:10 +0200

    array-object

--- js/app.js
+++ js/app.js
@@ -1,2 +1,44 @@
 'use strict';
 
+const teamMembers = [
+
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        profileImg: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        profileImg: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        profileImg: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        profileImg: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        profileImg: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        profileImg: 'barbara-ramos-grapich-designer.jpg'
+    }
+
+];
+
+
+console.log(teamMembers);
